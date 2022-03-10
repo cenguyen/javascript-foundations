940b259e94fade9eb1db84aa299a75189e8bd9e8
Christine <cenguyen9@gmail.com>
2022-03-10 17:13:56 -0600

    created Centaur class and passed name test

--- mythical-creatures/test/centaur-test.js
+++ mythical-creatures/test/centaur-test.js
@@ -3,19 +3,27 @@ var Centaur = require('../exercises/centaur');
 
 describe('Centaur', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     assert.isFunction(Centaur);
   });
 
-  it.skip('should have a name', function() {
+  it('should have a name', function() {
     // instantiate a Centaur object with an object as an argument. The object should have a key of name and value of your name
     // instantiate another Centaur object with a different name
-
+    var centaur = new Centaur('Firenze');
+  
+      assert.equal(centaur.name, 'Firenze');
+  });
+  
+  it('should be able to have a different name', function() {
+    var centaur = new Centaur('Robert');
+  
+      assert.equal(centaur.name, 'Robert');
+  });
     // assert that the first centaur's name is your name
     // assert that the second centaur's name is the other name
 
     //NOTE: We will not ask you to write your own tests in a mod 1 assessment, but it is good practice to prepare for mod 2!
-  });
 
   it.skip('should have a breed', function() {
     var centaur = new Centaur({ name: 'Georgiana', type: 'Palomino' });
